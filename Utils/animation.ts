0447b5b3ab8cf45c75de5daefa533b73c8c2c320
CCZX <ccdebuging@gmail.com>
2021-03-29 18:38:57 +0800

    animation function

--- Utils/animation.ts
+++ Utils/animation.ts
@@ -6,6 +6,37 @@
  * 
  * 滚动函数
  */
-function scrollTop() {
-  
+export function smoothScrollTo(
+  el,
+  direction,
+  position,
+  duration = 350,
+) {
+  const v0 = direction === 'top' ? el.scrollTop : el.scrollLeft;
+  const dScroll = position - v0;
+  let t0 = 0;
+  function raf(t) {
+    if (t0 === 0) {
+      t0 = t;
+      requestAnimationFrame(raf);
+      return;
+    }
+    const p = (t - t0) / 350;
+    const v1 = v0 + Math.ceil(p * p * (3 - 2 * p) * dScroll); // Bézier
+    if (direction === 'top') {
+      el.scrollTop = v1;
+    } else {
+      el.scrollLeft = v1;
+    }
+    if (p < 1) {
+      requestAnimationFrame(raf);
+    } else {
+      if (direction === 'top') {
+        el.scrollTop = position;
+      } else {
+        el.scrollLeft = position;
+      }
+    }
+  }
+  requestAnimationFrame(raf);
 }
